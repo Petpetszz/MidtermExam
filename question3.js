d5807cd15a97bc648a6d439714d6baf004b8689c
unknown <YY210@ccs.local>
2025-03-10 20:32:58 +0800

    Question4 and Slight changes to Question3

--- question3.js
+++ question3.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/test', (req, res) => {
-    res.json({ message: 'Express is working! Write your full name' });
+    res.json({ message: 'Express is working! John Miguel Cabantugan' });
 });
 
 app.listen(port, () => {
